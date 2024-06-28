ea28665f8caadc725de143358b243942ba217752
BagasWihant <bagaswihant@gmail.com>
2024-06-28 16:36:43 +0700

    stylish text generate

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,8 @@ const Hero = () => {
             <div>
                 <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
                 <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="blue" />
-                <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="purple" />
+                <Spotlight className="top-80 left-full h-[80vh] w-[50vw]" fill="yellow" />
+                <Spotlight className="top-60 left-10 h-screen " fill="purple" />
             </div>
 
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.5] absolute top-0 left-0 flex items-center justify-center">
@@ -19,12 +20,14 @@ const Hero = () => {
             <div className="flex justify-center relative my-20 z-10">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                        Bagas Wihantoro
+                        Let's Build Something
                     </h2>
 
                     <TextGenerateEffect
                         className="text-center text-4xl md:text-6xl lg:text-7xl"
                         words="Software Engineer & Web Developer" />
+
+                        <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I'm Bagas Wihantoro, a software engineer and web developer</p>
                 </div>
             </div>
         </div>
